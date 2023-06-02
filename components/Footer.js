a5a8c164f7be6225c04b29b162d302555c756a33
Pulido <eduardo.pulido@cceo.com.mx>
2023-06-01 21:50:42 -0600

    Primer commit

--- components/Footer.js
+++ components/Footer.js
@@ -4,29 +4,27 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className="bg-black py-10 mt-20 text-white">
-     <div className="contenedor text-center items-center">
-     <div className="flex items-center justify-start">
-  <Link href="/">
-    <Image width={200} height={100} src="/img/Echoes-logo-w.png" />
-  </Link>
-</div>
-
-        
-     <nav className="flex justify-center items-center space-x-4">
-  <Link href="/" className="text-white  hover:text-gray-400 no-underline">Inicio</Link>
-  <Link href="/" className="text-white  hover:text-gray-400 no-underline">Ayuda</Link>
-  <Link href="/" className="text-white  hover:text-gray-400 no-underline">Términos y condiciones</Link>
-</nav>
+      <div className="contenedor text-center items-center">
+        <div className="flex items-center justify-start">
+          <Link href="/">
+            <Image width={200} height={100} src="/img/Echoes-logo-w.png" />
+          </Link>
+        </div>
 
 
-       
+        <nav className="flex justify-center items-center space-x-4">
+          <Link href="/" className="text-white  hover:text-gray-400 no-underline">Inicio</Link>
+          <Link href="/" className="text-white  hover:text-gray-400 no-underline">Ayuda</Link>
+          <Link href="/" className="text-white  hover:text-gray-400 no-underline">Términos y condiciones</Link>
+        </nav>
+
         <div className="mt-12 text-center">
           <p className="text-sm">&copy; {new Date().getFullYear()} Echoes. Todos los derechos reservados.</p>
           <p className="text-sm">UPA, AGS, MEX</p>
         </div>
-     </div>
+      </div>
     </footer>
-  
+
   );
 };
 
