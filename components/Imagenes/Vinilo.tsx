5ea3c375d8ce5dd4cfb0481067a3378d93920fb3
Jaime Jesabel <jaime_jesabel@hotmail.com>
2023-07-19 00:08:53 -0600

    cambios

---
+++ components/Imagenes/Vinilo.tsx
@@ -0,0 +1,17 @@
+'use client'
+import Image from "next/image";
+
+const Vinilo = () => {
+    return (
+        <div className="relative overflow-hidden rounded-xl  aspect-square lg:aspect-auto  w-full h-full">
+            <Image
+                className="object-cover"
+                src="/img/Inicio/Vinilo.jpeg"
+                alt="First slide"
+                fill
+            />
+        </div>
+    );
+}
+
+export default Vinilo;
